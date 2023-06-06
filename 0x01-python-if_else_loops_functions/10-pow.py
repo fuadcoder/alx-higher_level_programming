eebffa7c663e75307fb375eac3025bb8ab27e842
Horlams <olasupoolamide00@gmail.com>
2023-06-06 10:55:11 -0700

    python if_else_loops functions

---
+++ 0x01-python-if_else_loops_functions/10-pow.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Olamide Olasupo
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author - Olamide Olasupo
+
+def print_last_digit(number):
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
